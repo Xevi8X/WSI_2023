e0d0a3be42c3c4287617e81076b18d3b285fbd77
jaBrzosko <jakubbrzoskowski@gmail.com>
2023-05-15 15:42:47 +0200

    Some changes

--- ai/predictSingle.py
+++ ai/predictSingle.py
@@ -17,10 +17,12 @@ def predict(stock_path: str, inputModelPath: str, numberOfDays: int) -> list:
     data = prepare_test_data(stock_path, NUMBER_OF_DAYS, LOOKUP_STEP)
     model = tf.keras.models.load_model(inputModelPath)
     y_pred = model.predict(data['X_test'])
+
+    y_test = np.squeeze(data["column_scaler"]["Close"].inverse_transform(np.expand_dims(data['Y_test'], axis=0)))
     y_pred = np.squeeze(data["column_scaler"]["Close"].inverse_transform(y_pred))
-    return y_pred[-numberOfDays:]
+    return y_test[-numberOfDays:], y_pred[-numberOfDays:]
 
 if __name__ == "__main__":
-    predict = predict_single(sys.argv[1], sys.argv[2])
+    real, predict = predict(sys.argv[1], sys.argv[2], 30)
     for i, pred in enumerate(predict):
-        print(f"For day in the future number {i + 1} predicted closing price is: {pred}")
+        print(f"For day in the future number {i + 1} predicted closing price is: {pred}, real price is {real[i]}")
